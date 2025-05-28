105df636d845d407004d26083c81c73cdb4c5060
Omy10 <naomimwangi382002@gmail.com>
2025-05-28 12:24:34 +0300

    Arguments

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2);
+if (args.lenght === 0){
+    console.log("no argument found")
+} 
+else if (args.lenght === 1){
+    console.log("argument found");
+}else{
+    console.log("argument found")
+}
